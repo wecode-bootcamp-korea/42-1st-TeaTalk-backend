808e5bd15180e96e46ea9627a278d25729c6d1d5
박준우 <junwoo@bagjun-uui-MacBookPro.local>
2023-02-14 10:21:57 +0900

    [42기 박준우] 제품조회페이지 쿼리빌더 수정

--- models/productDao.js
+++ models/productDao.js
@@ -2,27 +2,47 @@ const { teaDataSource } = require("./datasource.js");
 
 const getProducts = async (start, pageSize, category, sub, typeArr, sort) => {
   let whereCondition = category ? `WHERE` : typeArr ? `WHERE` : "";
+  let subAndCondition = category ? `AND` : "";
   let andCondition = category && typeArr ? `AND` : sub && typeArr ? `AND` : "";
   const categoryCondition = category ? `c.name='${category}'` : "";
-  const subCondition = sub ? `AND s.name='${sub}'` : "";
+  const subCondition = sub ? `s.name='${sub}'` : "";
+
   const typeCondition = typeArr
     ? `INNER JOIN product_types t ON p.product_type_id = t.id`
     : "";
-  const typeArrCondition = typeArr[0]
-    ? typeArr[1]
-      ? typeArr[2]
-        ? typeArr[3]
-          ? `(t.type='${typeArr[0]}' OR t.type='${typeArr[1]}' OR t.type='${typeArr[2]}',t.type='${typeArr[3]}')`
-          : `(t.type='${typeArr[0]}' OR t.type='${typeArr[1]}' OR t.type='${typeArr[2]}')`
-        : `(t.type='${typeArr[0]}' OR t.type='${typeArr[1]}')`
-      : `(t.type='${typeArr[0]}')`
-    : "";
-  const sortSet = {
-    new_arrival: `ORDER BY p.created_at DESC`,
-    price_desc: `ORDER BY p.discount_price DESC`,
-    price_asc: `ORDER BY p.discount_price ASC`,
-  };
-  const sortCondition = sortSet[sort];
+  let typeArrCondition = "";
+  for (let i = 0; i < typeArr.length; i++) {
+    if (i === 0) {
+      typeArrCondition = typeArrCondition + `(t.type='${typeArr[i]}'`;
+    } else {
+      typeArrCondition = typeArrCondition + ` OR t.type='${typeArr[i]}'`;
+    }
+    if (i === typeArr.length - 1) {
+      typeArrCondition = typeArrCondition + `)`;
+    }
+  }
+  let sortCondition = "";
+  switch (sort) {
+    case "new_arrival":
+      sortCondition = `ORDER BY p.created_at DESC`;
+      break;
+    case "price_desc":
+      sortCondition = `ORDER BY p.discount_price DESC`;
+      break;
+    case "price_asc":
+      sortCondition = `ORDER BY p.discount_price ASC`;
+      break;
+    default:
+      sortCondition = "";
+      break;
+  }
+  console.log(typeCondition);
+  console.log(whereCondition);
+  console.log(categoryCondition);
+  console.log(subCondition);
+  console.log("and", andCondition);
+  console.log("type:", typeArrCondition);
+  console.log("이거냐:", sortCondition);
   return await teaDataSource.query(
     `SELECT
         p.name,
@@ -37,6 +57,7 @@ const getProducts = async (start, pageSize, category, sub, typeArr, sort) => {
         ${typeCondition}
         ${whereCondition}
         ${categoryCondition}
+        ${subAndCondition}
         ${subCondition}
         ${andCondition}
         ${typeArrCondition}
