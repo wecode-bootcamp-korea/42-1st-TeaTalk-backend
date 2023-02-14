f0e338560d829f263419f849b5d03eb0a5d5f4b2
박준우 <junwoo@bagjun-uui-MacBookPro.local>
2023-02-14 16:37:19 +0900

    [42기 박준우] 제품상세페이지 sql문 컨벤션 지키기!

--- models/productDao.js
+++ models/productDao.js
@@ -11,12 +11,12 @@ const getProductById = async (productId) => {
       JSON_ARRAYAGG(i.image_url) as detail_image,
       s.name AS subcategory_name,
       c.name AS category_name
-      FROM products p
-      INNER JOIN product_images i ON p.id = i.product_id
-      INNER JOIN subcategories s ON p.subcategory_id = s.id
-      INNER JOIN categories c ON s.category_id = c.id
-      WHERE p.id = ?
-      GROUP BY p.id;`,
+    FROM products p
+    INNER JOIN product_images i ON p.id = i.product_id
+    INNER JOIN subcategories s ON p.subcategory_id = s.id
+    INNER JOIN categories c ON s.category_id = c.id
+    WHERE p.id = ?
+    GROUP BY p.id;`,
     [productId]
   );
 };
