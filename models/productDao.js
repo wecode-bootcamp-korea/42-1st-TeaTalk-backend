05a4d5552d166218b86838cd409a66e02830bf4e
박준우 <junwoo@bagjun-uui-MacBookPro.local>
2023-02-15 17:12:44 +0900

    [42기 박준우] 제품리스트 조회 부분 포매팅 수정 및 product id와 product_images 테이블의 id, url 값 반환' ;

--- models/productDao.js
+++ models/productDao.js
@@ -44,25 +44,42 @@ const getProducts = async (start, limit, category, sub, typeArr, sort) => {
       sortCondition = "";
       break;
   }
+  console.log(typeCondition);
+  console.log(whereCondition);
+  console.log(categoryCondition);
+  console.log(subCondition);
+  console.log(andCondition);
+  console.log(typeArrCondition);
+  console.log(sortCondition);
   return await teaDataSource.query(
     `SELECT
-        p.name,
-        p.price,
-        p.discount_price
-        FROM
-        products AS p
-        INNER JOIN subcategories AS s
-        ON p.subcategory_id = s.id
-        INNER JOIN categories AS c
-        ON s.category_id = c.id
-        ${typeCondition}
-        ${whereCondition}
-        ${categoryCondition}
-        ${subCondition}
-        ${andCondition}
-        ${typeArrCondition}
-        ${sortCondition}
-        LIMIT ?,?`,
+        p.id AS product_id,
+        p.name AS product_name,
+        p.price AS product_price,
+        p.discount_price AS product_discount_price, 
+        pi.images
+      FROM products AS p
+      LEFT JOIN(
+        SELECT
+          product_id,
+          JSON_ARRAYAGG(
+            JSON_OBJECT(
+              'id', id,
+              'url', image_url
+            )
+          ) AS images
+        FROM product_images GROUP BY product_id
+      ) AS pi ON p.id = pi.product_id
+      INNER JOIN subcategories AS s ON p.subcategory_id = s.id
+      INNER JOIN categories AS c ON s.category_id = c.id
+      ${typeCondition}
+      ${whereCondition}
+      ${categoryCondition}
+      ${subCondition}
+      ${andCondition}
+      ${typeArrCondition}
+      ${sortCondition}
+      LIMIT ?,?`,
     [start, limit]
   );
 };
