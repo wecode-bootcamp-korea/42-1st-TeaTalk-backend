8debec8a3544efd95687fb3e6771a9876e86f3e2
박준우 <junwoo@bagjun-uui-MacBookPro.local>
2023-02-15 15:34:31 +0900

    [42기 박준우] app.js 코드 위치 변경

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const routes = require("./routes");
 
+const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
@@ -17,6 +16,7 @@ app.get("/ping", (req, res) => {
   res.status(200).json({ message: "pong" });
 });
 
+const PORT = process.env.PORT;
 const start = async () => {
   app.listen(PORT, () => {
     console.log(`server is listening on ${PORT}`);
