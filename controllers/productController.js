ee408ff4fc46f7be99c4bbe7e14300f32f86045d
박준우 <junwoo@bagjun-uui-MacBookPro.local>
2023-02-16 14:50:10 +0900

    [42기 박준우] Add: 제품 리스트 조회 API 구현

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,21 @@
 const productService = require("../services/productService");
 
+const getProductList = async (req, res) => {
+  const { categoryId, subCategoryId, type, sort, page = 1 } = req.query;
+  try {
+    const lists = await productService.getProductList(
+      +page,
+      categoryId,
+      subCategoryId,
+      type,
+      sort
+    );
+    res.status(200).json({ data: lists });
+  } catch (err) {
+    return res.status(err.status || 500).json({ message: err.message });
+  }
+};
+
 const getProductById = async (req, res) => {
   try {
     const { productId } = req.params;
@@ -16,5 +32,6 @@ const getProductById = async (req, res) => {
 };
 
 module.exports = {
+  getProductList,
   getProductById,
 };
--- services/productService.js
+++ services/productService.js
@@ -1,10 +1,27 @@
 const productDao = require("../models/productDao");
 
+const getProductList = async (page, categoryId, subCategoryId, type, sort) => {
+  let offset;
+  page ? (offset = (page - 1) * 8) : (offset = 0);
+
+  const typeArr = type ? type.split(",") : "";
+
+  const result = await productDao.getProductList(
+    offset,
+    categoryId,
+    subCategoryId,
+    typeArr,
+    sort
+  );
+
+  return result;
+};
+
 const getProductById = async (productId) => {
-  const productDetail = await productDao.getProductById(productId);
-  return productDetail;
+  return await productDao.getProductById(productId);
 };
 
 module.exports = {
+  getProductList,
   getProductById,
 };
